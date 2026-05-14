20b7ed2b5e13b868ba33c6bf9531500457b7cc82
Nathan Kebede <nathanzelalem100@gmail.com>
2026-05-14 15:24:49 +0300

    remove maintainance code in db migration

--- src/main/resources/db/migration/V5__create_index_concurrently.sql
+++ src/main/resources/db/migration/V5__create_index_concurrently.sql
@@ -1,6 +1,3 @@
 -- flyway:noTransaction=true
 CREATE INDEX CONCURRENTLY idx_messages_poll_lean 
     ON public.messages (queue_id, created_at ASC);
--- Maintenance (These are also non-transactional)
-VACUUM public.messages;
-ANALYZE public.messages;
